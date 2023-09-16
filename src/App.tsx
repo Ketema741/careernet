529909dedef8cbb87dfe9ce56f2352d5cd7b06e1
Ketema Girma <ketema.girma@a2sv.org>
2023-09-16 16:49:31 +0300

    add router layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import AboutUs from './pages/aboutus/AboutUs';
 import FAQ from './pages/faq/FAQ';
+import {Layout} from './components';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/faq" element={<FAQ />} />
-        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/aboutus" element={<AboutUs />} />
+        </Route>
 
       </Routes>
     </Router>
