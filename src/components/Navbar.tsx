263a85d8435c1779b0d93b8de8784a3e0268373e
Ketema Girma <ketema.girma@a2sv.org>
2023-09-19 11:59:55 +0300

    add guest link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,45 +1,75 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { useEffect, Fragment } from 'react';
+import { Link } from 'react-router-dom';
 import { AiOutlineMenu } from 'react-icons/ai';
-import { BsChatLeft } from 'react-icons/bs';
 import { RiNotification3Line } from 'react-icons/ri';
 import { MdKeyboardArrowDown } from 'react-icons/md';
+
 import { RxAvatar } from 'react-icons/rx';
+import Notification from './Notification';
+import Profile from './Profile';
+
+import { useAuthContext } from "../context/Auth/authContext";
 import { useStateContext } from '../context/ContextProvider';
+
+const GuestLinks = (
+  <div className="flex justify-center items-center gap-x-5">
+    <Link className="pt-2 cursor-pointer hover:bg-light-gray rounded-lg" to="/register">
+      Register
+    </Link>
+
+    <Link className=" pt-2 pr-5 cursor-pointer hover:bg-light-gray rounded-lg" to="/login">
+      Login
+    </Link>
+  </div>
+);
 
 interface NavButtonProps {
   title: string;
   customFunc: () => void;
   icon: React.ReactNode;
   color: string;
+  dotColor?: string;
 }
 
-const NavButton: React.FC<NavButtonProps> = ({ title, customFunc, icon, color }) => (
+const NavButton: React.FC<NavButtonProps> = ({ title, customFunc, icon, color, dotColor }) => (
   <button
     type="button"
     onClick={() => customFunc()}
     style={{ color }}
     className="relative text-xl rounded-full p-3 hover:bg-light-gray"
   >
+    {dotColor && (
+      <span
+        style={{ background: dotColor }}
+        className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
+      />
+    )}
     {icon}
   </button>
 );
 
 const Navbar: React.FC = () => {
+  const { isUserAuthenticated, logout } = useAuthContext();
 
   const {
     currentColor,
     activeMenu,
     setActiveMenu,
     handleClick,
+    isClicked,
     setScreenSize,
     screenSize,
   } = useStateContext();
 
   useEffect(() => {
     const handleResize = () => setScreenSize(window.innerWidth);
+
     window.addEventListener("resize", handleResize);
+
     handleResize();
+
     return () => window.removeEventListener("resize", handleResize);
+   
     // eslint-disable-next-line
   }, []);
 
@@ -49,9 +79,13 @@ const Navbar: React.FC = () => {
     } else {
       setActiveMenu(true);
     }
+
     // eslint-disable-next-line
   }, [screenSize]);
 
+  const onLogout = () => {
+    logout();
+  };
 
   const handleActiveMenu = () => setActiveMenu(!activeMenu);
 
@@ -65,33 +99,31 @@ const Navbar: React.FC = () => {
       />
 
       <div className="flex">
-        <Fragment>
-          <div className="hidden sm:block">
-            <NavButton
-              title="Chat"
-              customFunc={() => handleClick("chat")}
-              color={currentColor}
-              icon={<BsChatLeft />}
-            />
-          </div>
+        {isUserAuthenticated ? (
+          <Fragment>
+            <div className="hidden sm:block">
+              <NavButton
+                title="Notification"
+                dotColor="#FF5C8E"
+                customFunc={() => handleClick("notification")}
+                color={currentColor}
+                icon={<RiNotification3Line />}
+              />
+            </div>
+            <div
+              className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg"
+              onClick={() => handleClick("userProfile")}
+            >
+              <RxAvatar className="rounded-full w-7 h-7" />
+              <MdKeyboardArrowDown className="text-gray-400 text-14" />
+            </div>
 
-          <div className="hidden sm:block">
-            <NavButton
-              title="Notification"
-              customFunc={() => handleClick("notification")}
-              color={currentColor}
-              icon={<RiNotification3Line />}
-            />
-          </div>
-          <div
-            className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg"
-            onClick={() => { }}
-          >
-            <RxAvatar className="rounded-full w-7 h-7" />
-            <MdKeyboardArrowDown className="text-gray-400 text-14" />
-          </div>
-        </Fragment>
-
+            {isClicked.notification && <Notification />}
+            {isClicked.userProfile && <Profile onLogout={onLogout} />}
+          </Fragment>
+        ) : (
+          GuestLinks
+        )}
       </div>
     </div>
   );
